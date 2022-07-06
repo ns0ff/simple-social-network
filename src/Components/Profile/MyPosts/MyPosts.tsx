40ddf75242cf7b803f9eeba2b115d44044ddb238
Nick <74413828+ns0ff@users.noreply.github.com>
2022-07-07 01:15:12 +0300

    part 4 (step 31)

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -16,12 +16,18 @@ export const MyPosts = (props: MyPostsType) => {
 
     const postsElements = props.postData.map(el => <Post id={el.id} message={el.message} likes={el.likes} />)
 
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+    const addPost = () => {
+        let text = newPostElement.current?.value
+        alert(text)
+    }
+
     return (
         <div className={styles.postsBlock}>
             My posts
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <textarea ref={newPostElement}></textarea>
+                <button onClick={addPost}>Add post</button>
                 <button>Remove</button>
             </div>
             <div className={styles.posts}>

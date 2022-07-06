57a273240b5d12551f1c0176b51093246d26c413
Nick <74413828+ns0ff@users.noreply.github.com>
2022-07-06 06:59:07 +0300

    part 2 (step 21) dialog page added

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import style from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            <h1>Dialogs</h1>
+        <div className={style.dialogs}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog + ' ' + style.active }>
+                    Marius Hutchinson
+                </div>
+                <div className={style.dialog}>
+                    Jaimee Bryan
+                </div>
+                <div className={style.dialog}>
+                    Tess Coffey
+                </div>
+                <div className={style.dialog}>
+                    Myles Atherton
+                </div>
+                <div className={style.dialog}>
+                    Crystal Heaton
+                </div>
+                <div className={style.dialog}>
+                    Veer Cotton
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>Qui ad sequi tempore dolorem nihil. Vel aliquam exercitationem temporibus et incidunt. Voluptate itaque qui suscipit natus reiciendis deserunt. Est nulla accusantium autem.</div>
+                <div className={style.message}>Sapiente saepe quos et labore. Pariatur sunt est sit quis. Et hic quidem ratione.</div>
+            </div>
         </div>
+
+
     );
 };
